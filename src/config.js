a44e9d343678facd0dc5991f015bdcd30e2995e3
Vasile NISTOR <vasile.nistor@estiam.com>
2019-11-25 13:26:00 +0100

    first

---
+++ src/config.js
@@ -0,0 +1,4 @@
+let tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c66c31dc-639e-4d4f-9f60-4b46827b6b66/token";
+let instanceLocator = "v1:us1:c66c31dc-639e-4d4f-9f60-4b46827b6b66";
+
+export {tokenUrl, instanceLocator}
